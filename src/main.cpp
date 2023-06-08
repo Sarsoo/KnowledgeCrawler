e6a5581ceaf1fda9a80733aaed650e558f8c4916
Andy Pack <andy@sarsoo.xyz>
2023-06-08 17:57:19 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <memory>
+
+#include "logging.cpp"
+#include "config.cpp"
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+    try{
+        init_logging();
+
+        BOOST_LOG_TRIVIAL(info) << "================================";
+        BOOST_LOG_TRIVIAL(info) << "   kc";
+        BOOST_LOG_TRIVIAL(info) << "================================";
+        BOOST_LOG_TRIVIAL(info) << "starting up....";
+
+        auto config = init_config(argc, argv);
+
+        if(config)
+        {
+            BOOST_LOG_TRIVIAL(info) << "hello world";
+        }
+    }
+    catch (const po::error &ex)
+    {
+        BOOST_LOG_TRIVIAL(error) << ex.what();
+    }
+}
